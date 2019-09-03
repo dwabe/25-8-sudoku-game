becb9eefc2be5d53c018d436359192a53806eeff
Bartek Błęcki <bartek@blecki.pl>
2019-09-03 11:45:57 +0200

    Add Board.js

---
+++ src/components/Board.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import style from './Board.css';
+import Tile from './Tile.js';
+
+class Board extends React.Component {
+  numbers (val) {
+    const tileValue = val.target.value;
+    const tileID = val.target.id;
+    const newVal = tileValue <= max ? tileValue : parseInt(tileValue.toString().substring(0, maxLength));
+
+    this.props.onBoardUpdate(tileID, newVal);
+  }
+
+  render() {
+    let items = [];
+
+    if (this.props.data) {
+      items = this.props.data.map((item, index) =>
+        <Tile value={item} action={this.numbers.bind(this)} id={index} key={index}/>
+      );
+    }
+
+    return (
+      <>
+        <div className={style.Board}>
+          {items}
+        </div>
+      </>
+    )
+  }
+}
+
+export default Board;
